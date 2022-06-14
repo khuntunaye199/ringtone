d967c7b997d3d00c82782e3ae1dcbff8b7b2069d
khuntunaye199 <khuntunaye.kmkk@gmail.com>
2022-06-14 10:27:49 +0630

    ringtonetest

---
+++ example/build/app/generated/source/buildConfig/debug/acr/rt/ringtone_set_example/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package acr.rt.ringtone_set_example;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "acr.rt.ringtone_set_example";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
---
+++ example/build/ringtone_set/generated/source/buildConfig/debug/acr/rt/ringtone_set/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package acr.rt.ringtone_set;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "acr.rt.ringtone_set";
+  public static final String BUILD_TYPE = "debug";
+}
